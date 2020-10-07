a52dfad03fb31fdc8fdf3da3cfd9d52eafc51f6c
izzyuri <uriegasisrael@gmail.com>
2020-10-07 01:41:21 -0500

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-
+  #  array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
+    return array.join(" and ") if array.count < 3
+    [array[0..-2].join(", "), array.last].join(", and ")
 end
+# puts oxford_comma(1)
